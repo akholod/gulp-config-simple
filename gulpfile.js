b4818d9d83124b61cd0bf102c3a47ed3d71f1cfa
Andrey Kokhanevich <andrey@itservices.in.ua>
2018-06-07 18:10:32 +0300

    changes added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const runSequence = require('run-sequence');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const minifycss = require('gulp-cssnano');
+const pug = require('gulp-pug');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const del = require('del');
@@ -35,14 +36,14 @@ gulp.task('sass', function () {
 gulp.task('styles', ['sass'], function () {
   return gulp.src(paths.css)
     .pipe(concat('main.css'))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('./serve/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
 gulp.task('build_styles', ['styles'], function () {
-  return gulp.src('./app/css/main.css') 
+  return gulp.src('./serve/css/main.css') 
     .pipe(minifycss())
     .pipe(gulp.dest('./dist/css'))
 });
@@ -53,41 +54,58 @@ gulp.task('build_styles', ['styles'], function () {
 gulp.task('scripts', function () {
   return gulp.src(paths.js)
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./app/js'))
+    .pipe(gulp.dest('./serve/js'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
 gulp.task('build_scripts', ['scripts'], function () {
-  return gulp.src('./app/js/main.js')
+  return gulp.src('./serve/js/main.js')
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'))
 });
 ///////////
 
-gulp.task('html', function () {
-  return gulp.src('app/**/*.html')
-    .pipe(gulp.dest('./dist'));
-});
+// gulp.task('html', function () {
+//   return gulp.src('app/**/*.html')
+//     .pipe(gulp.dest('./dist'));
+// });
 
-gulp.task('images', function () {
+gulp.task('build_images', function () {
   return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
     .pipe(cache(imagemin({
       interlaced: true
     })))
-    .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('./dist/images'))
+});
+
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(gulp.dest('./serve/images'))
 });
 
 gulp.task('fonts', function () {
   return gulp.src('app/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'))
+    .pipe(gulp.dest('./serve/fonts'))
 })
+
+gulp.task('pug', function buildHTML() {
+  return gulp.src('app/templates/pages/**/*.pug')
+  .pipe(pug({
+    locals: {},
+    pretty: true
+  }))
+  .pipe(gulp.dest('serve'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+});
 
 gulp.task('browserSync', function () {
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir: 'serve'
     },
   })
 })
@@ -96,10 +114,18 @@ gulp.task('clean:dist', function () {
   return del.sync('dist');
 })
 
-gulp.task('watch', ['browserSync', 'styles', 'scripts'], function () {
+gulp.task('clean:serve', function () {
+  return del.sync('dist');
+})
+
+gulp.task('preWatch', )
+
+gulp.task('watch', ['clean:serve', 'browserSync', 'fonts', 'images', 'styles', 'scripts', 'pug'], function (callback) {
   gulp.watch('app/scss/**/*.scss', ['styles']);
+  gulp.watch('app/img/**/*.+(png|jpg|jpeg|gif|svg)', ['images']);
   gulp.watch('app/js/*.js', ['scripts']);
-  gulp.watch('app/**/*.html', browserSync.reload);
+  gulp.watch('app/templates/**/*.pug', ['pug']);
+  gulp.watch('app/html/**/*.html', browserSync.reload);
 });
 
 gulp.task('build', function (callback) {
